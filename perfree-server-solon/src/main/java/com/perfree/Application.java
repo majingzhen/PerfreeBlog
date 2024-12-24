96d3ff9c3a52a3d8f6f7d12da4db536e758ee0ce
马图图 <matuto@qq.com>
2024-12-24 23:03:46 +0800

    1.修改配置类及启动类

--- perfree-server-solon/src/main/java/com/perfree/Application.java
+++ perfree-server-solon/src/main/java/com/perfree/Application.java
@@ -1,22 +1,21 @@
 package com.perfree;
 
+import org.noear.solon.Solon;
+import org.noear.solon.SolonApp;
+import org.noear.solon.annotation.SolonMain;
+import org.noear.solon.scheduling.annotation.EnableAsync;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
-import org.springframework.scheduling.annotation.EnableAsync;
+
 
 /**
  * @author Perfree
  * @description Application: 程序入口
  * @date 15:41 2023/9/28
  */
-@SpringBootApplication
-@ComponentScan(nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class)
 @EnableAsync
+@SolonMain
 public class Application {
-
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        Solon.start(Application.class, args);
     }
 }
